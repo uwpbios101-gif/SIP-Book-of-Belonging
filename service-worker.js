bfd2ba6c57f5d36fb0b871b47bd5625d43e52519
selassiefest-web <selassiefest@gmail.com>
2026-07-11 19:14:26 -0500

    font jost 2

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE='sip-book-of-belonging-v1-3-jost-title';
+const CACHE='sip-book-of-belonging-v1-4-title-size-color';
 const CORE=[
   "./",
   "./index.html",
-  "./assets/css/styles.css?v=1.3.0",
-  "./assets/js/app.js?v=1.3.0",
+  "./assets/css/styles.css?v=1.4.0",
+  "./assets/js/app.js?v=1.4.0",
   "./assets/data/campaigns.json",
-  "./manifest.webmanifest?v=1.3.0",
+  "./manifest.webmanifest?v=1.4.0",
   "./assets/img/sip-logo.png",
   "./assets/img/icon-192.png",
   "./assets/img/icon-512.png",
